ab2511417d29db53982242a8647ce8ce57342aae
Henry-Julian <hjqg689@gmail.com>
2023-01-28 19:37:20 -0500

    //

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let nombre_gato = "manchas";
 let edad = 2;
 let macho = true;
 let hembra = false;
-var propietario = { nombre: "Henry", edad: "33", ciudad : "Bucaramanga" };
+//var propietario = { nombre: "Henry", edad: "33", ciudad : "Bucaramanga" };
 
 console.log(`El nombre del gato es ${nombre_gato}`);
 console.log(`La edad del gato ${edad}`);
