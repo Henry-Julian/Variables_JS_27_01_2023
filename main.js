86bcfa658ba7ac0ad2cf516b8eeb22fd20052e5d
Henry-Julian <hjqg689@gmail.com>
2023-01-28 20:06:52 -0500

    Actualizando datos

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let hembra = false;
 let propietario = { nombre: "Henry", edad: "33", ciudad : "Bucaramanga" };
 
 console.log(`El nombre del gato es ${nombre_gato}`);
-console.log(`La edad del gato ${edad}`);
+console.log(`La edad del gato en años es de ${edad}`);
+console.log("Si el gato es macho va la palabra: true, si el gato es una hembra es false");
 console.log(`El género del gato es ${macho}`);
 console.log(`El propietario del gato es ${propietario.nombre}`);
 console.log(`La edad del propietario del gato es ${propietario.edad}`);
