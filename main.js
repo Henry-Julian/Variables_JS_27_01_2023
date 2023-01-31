d0ad229d66b17d4b7a19ecd520308777eabd6b41
Henry-Julian <hjqg689@gmail.com>
2023-01-30 19:49:08 -0500

    Datos en la consola

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-let nombre_gato = "manchas";
-let edad = 2;
-let macho = true;
-let hembra = false;
-let propietario = { nombre: "Henry", edad: "33", ciudad : "Bucaramanga" };
+let nombre = "Henry Julián Quintero Gutiérrez";
 
-console.log(`El nombre del gato es ${nombre_gato}`);
-console.log(`La edad del gato en años es de ${edad}`);
-console.log("Si el gato es macho va la palabra: true, si el gato es una hembra es false");
-console.log(`El género del gato es ${macho}`);
-console.log(`El propietario del gato es ${propietario.nombre}`);
-console.log(`La edad del propietario del gato es ${propietario.edad}`);
-console.log(`El propietario del gato vive en ${propietario.ciudad}`);
+console.log(nombre);
+console.warm(nombre);
+console.error(nombre);
+console.info(nombre);
+console.group("Mensajes agrupados con título");
+console.groupEnd();
+
+
+console.table([nombre]);
+console.table([{nombre_variable: nombre}]);
+
+
+console.clear();
+
+console.time();
+    console.log("sdmfklsjf");
+console.timeEnd();
+
+
+
 
